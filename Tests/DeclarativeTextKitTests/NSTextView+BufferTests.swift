c3f8745c6454052040c746f386aa73d39cd6e4b1
Christian Tietze <me@christiantietze.de>
2024-05-13 12:09:10 +0200

    add NSTextView Buffer conformance tests

---
+++ Tests/DeclarativeTextKitTests/NSTextView+BufferTests.swift
@@ -0,0 +1,37 @@
+//  Copyright © 2024 Christian Tietze. All rights reserved. Distributed under the MIT License.
+
+import XCTest
+import DeclarativeTextKit
+
+final class NSTextView_BufferTests: XCTestCase {
+    func textView(_ string: String) -> NSTextView {
+        let textView = NSTextView(usingTextLayoutManager: false)
+        textView.string = string
+        return textView
+    }
+
+    func testRange() {
+        XCTAssertEqual(textView("").range,
+                       .init(location: 0, length: 0))
+        XCTAssertEqual(textView("a").range,
+                       .init(location: 0, length: 1))
+        XCTAssertEqual(textView("hello\n\nworld").range,
+                       .init(location: 0, length: 12))
+        XCTAssertEqual(textView("💃🐞🏴‍☠️").range,
+                       .init(location: 0, length: 9))
+    }
+
+    func testCharacterAtLocation() {
+        let buffer = textView("bug 🐞")
+        let characters = (0..<5).map { buffer.unsafeCharacter(at: $0) }
+        XCTAssertEqual(characters, ["b", "u", "g", " ", "🐞"])
+    }
+
+    func testInsertContentAtLocation() {
+        let buffer = textView("hi")
+
+        buffer.insert("🐞 bug", at: 1)
+
+        XCTAssertEqual(buffer.string, "h🐞 bugi")
+    }
+}
