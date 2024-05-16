249d2365ae95be0658d45fedffa1f126a31b9b9c
Christian Tietze <me@christiantietze.de>
2024-05-16 09:40:55 +0200

    use MutableStringBuffer in functional test

--- Tests/DeclarativeTextKitTests/UseCaseTests.swift
+++ Tests/DeclarativeTextKitTests/UseCaseTests.swift
@@ -5,7 +5,7 @@ import DeclarativeTextKit
 
 final class UseCaseTests: XCTestCase {
     func testWrapSelectionInLines() {
-        let buffer: Buffer = textView("""
+        let buffer: MutableStringBuffer = """
 # Heading
 
 Text here. It is
@@ -13,7 +13,7 @@ not a lot of text.
 
 But it is nice.
 
-""")
+"""
         let selectedRange = Buffer.Range(location: 20, length: 11) // From line 3, "here", up to the next line.
 
         let commandCascade = Select(LineRange(selectedRange)) { lineRange in
@@ -32,10 +32,10 @@ But it is nice.
 
         buffer.insert("raw")  // Simulate typing at the selection
 
-        XCTAssertEqual(buffer.content, """
+        XCTAssertEqual(buffer.description, """
 # Heading
 
-```raw
+```raw{^}
 Text here. It is
 not a lot of text.
 ```
