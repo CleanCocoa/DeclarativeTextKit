f2cd363bff238b3606ad73f12c638f540a352814
Christian Tietze <me@christiantietze.de>
2024-06-24 10:44:52 +0200

    fix typo in comment

--- Sources/DeclarativeTextKit/NSRange+isValidInsertionPointLocation.swift
+++ Sources/DeclarativeTextKit/NSRange+isValidInsertionPointLocation.swift
@@ -8,7 +8,7 @@ extension NSRange {
     /// - Return: `true` iff `location` is between or equal `lowerBound` or `upperBound`.
     @inlinable @inline(__always)
     public func isValidInsertionPointLocation(at location: Int) -> Bool {
-        // Insertion into an empty range at the 0 location, or in a non-empty range at the after-end position equal appending, and are permissible.
+        // Insertion into an empty range at the 0 location, or in a non-empty range at the after-end position represent an appending operation. Both are permissible.
         return location >= 0
             && lowerBound <= location
             && location <= upperBound
