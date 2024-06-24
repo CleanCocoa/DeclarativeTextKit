0511fd8bc9e30c8e6ef6b813fbd7d85e815309f0
Christian Tietze <me@christiantietze.de>
2024-06-24 10:45:27 +0200

    fix docstring to recognize 'returns:'

--- Sources/DeclarativeTextKit/NSRange+isValidInsertionPointLocation.swift
+++ Sources/DeclarativeTextKit/NSRange+isValidInsertionPointLocation.swift
@@ -5,7 +5,7 @@ import Foundation
 extension NSRange {
     /// Unlike `contains(_:)`, also returns `true` *at* the end position, which is not a valid position to read from, but a valid location to append content to.
     ///
-    /// - Return: `true` iff `location` is between or equal `lowerBound` or `upperBound`.
+    /// - Returns: `true` iff `location` is between or equal `lowerBound` or `upperBound`.
     @inlinable @inline(__always)
     public func isValidInsertionPointLocation(at location: Int) -> Bool {
         // Insertion into an empty range at the 0 location, or in a non-empty range at the after-end position represent an appending operation. Both are permissible.
